a0c36883a4cb95cf74cd29c7c94630a387597f05
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-17 15:35:33 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+         customerName= customerName.toLocaleUpperCase();
+         return customerName;
+     }
+
+
+var bestCustomer;
+function setBestCustomer(){
+    // bestCustomer = "not bob"
+    return bestCustomer="not bob";
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer ="maybe bob"
+    return bestCustomer;
+}
+
+// console.log(overwriteBestCustomer());
+
+const leastFavoriteCustomer ='Sang';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'kibet'
+
+}
